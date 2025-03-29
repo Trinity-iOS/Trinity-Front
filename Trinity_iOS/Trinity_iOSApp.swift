b684c6fe17288f8aff3309ad0277b9daed7717b7
30 <1224tpdud@gmail.com>
2025-03-29 16:32:00 +0900

    🛠 Set up initial Xcode project

---
+++ Trinity_iOS/Trinity_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trinity_iOSApp.swift
+//  Trinity_iOS
+//
+//  Created by Park Seyoung on 3/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Trinity_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Trinity_iOSTests/Trinity_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  Trinity_iOSTests.swift
+//  Trinity_iOSTests
+//
+//  Created by Park Seyoung on 3/29/25.
+//
+
+import Testing
+@testable import Trinity_iOS
+
+struct Trinity_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
